a6969245c97c2103ca33d50f668434c0dd51c00c
Shun Ueda <hasundue@gmail.com>
2025-05-20 19:04:01 +0900

    python: setup ruff

--- lua/lang/python.lua
+++ lua/lang/python.lua
@@ -9,3 +9,20 @@ require("core.lsp").setup("pyright", {
     },
   },
 })
+
+if vim.fn.executable("ruff") == 1 then
+  require("core.lsp").setup("ruff", {
+    settings = {
+      configurationPreferences = "filesystemFirst",
+      fixAll = true,
+      format = {
+        preview = true,
+      },
+      lint = {
+        enable = true,
+        preview = true,
+      },
+      organizeImports = true,
+    },
+  })
+end
