8863e113e5a0a0bbc18008154ef718d609108a13
Shun Ueda <hasundue@gmail.com>
2025-06-14 23:31:51 +0900

    lua: optimize lsp config

--- lua/lang/lua.lua
+++ lua/lang/lua.lua
@@ -1,16 +1,18 @@
 require("core.lsp").setup("lua_ls", {
   settings = {
     Lua = {
-      hint = {
-        enable = false,
+      runtime = {
+        version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
       },
       workspace = {
         checkThirdParty = false,
-        library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
-          "${3rd}/luv/library",
-          -- "${3rd}/busted/library",
-          -- "${3rd}/luassert/library",
-        }),
+        library = {
+          vim.api.nvim_get_runtime_file("lua", true),
+        },
       },
     },
   },
